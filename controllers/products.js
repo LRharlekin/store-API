01049b9ab2a65a1a0d5119731e99704ac863e574
LRharlekin <13718192+LRharlekin@users.noreply.github.com>
2023-12-26 22:24:45 +0200

    pagination, using skip + limit filters

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@
 const Product = require("../models/Product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select("name price");
+  const products = await Product.find({}).sort("name").select("name price");
   res.status(200).json({ products, numOfHits: products.length });
 };
 
@@ -34,6 +34,12 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(",").join(" ");
     result.select(fieldsList);
   }
+
+  // pagination logic
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
 
