90776111f1e211fada4e09d2c5b6f388a8eb735a
LRharlekin <13718192+LRharlekin@users.noreply.github.com>
2023-12-26 15:42:05 +0200

    connect to db

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
+const connectDB = require("./db/connect");
 
 const express = require("express");
 const app = express();
 
-// middleware
+const notFoundMiddleware = require("./middlewares/not-found");
+const errorHandlerMiddleware = require("./middlewares/error-handler");
+
+// middlewares
 app.use(express.static("/public"));
 app.use(express.json());
 
@@ -12,11 +16,18 @@ app.get("/", (req, res) => {
   res.status(200).send("Hello world");
 });
 
+// middlewares
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
 const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    // await DB connection
+    // await DB connection, and log when successful
+    await connectDB(process.env.MONGO_URI);
+    console.log("Connected to db...");
+
     app.listen(port, () => {
       console.log(`Listening on PORT:${port}...`);
     });
